180ac77a059ad96238170b9bc0a5395a727f1dac
robertkleinschuster <robert.kleinschuster@niceshops.com>
2020-10-18 18:43:14 +0200

    Refactor CmsSite and CmsParagraph to extend ArticleTranslation and Article

--- data/translation/validation/de_DE.php
+++ data/translation/validation/de_DE.php
@@ -10,4 +10,9 @@ return [
     'userstate.code.lock_self' => 'Aktuell angemeldeter Benutzer kann nicht gesperrt werden!',
     'person.firstname.empty' => 'Der Vorname darf nicht leer sein!',
     'person.lastname.empty' => 'Der Nachname darf nicht leer sein!',
+    'article.code.empty' => 'Der Code darf nicht leer sein!',
+    'article.code.unique' => 'Der Code ist bereits vergeben!',
+    'articletranslation.name.empty' => 'Der Name darf nicht leer sein!',
+    'articletranslation.code.empty' => 'Der URL-Code darf nicht leer sein!',
+    'articletranslation.code.unique' => 'Der URL-Code ist bereits vergeben!',
 ];
