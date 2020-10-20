5f2d32185836e8b76407ac12a0f0121c07225261
robertkleinschuster <robert.kleinschuster@niceshops.com>
2020-10-20 23:47:16 +0200

    Order for CmsMenu and Locale

--- src/Backoffice/src/Mvc/Index/IndexController.php
+++ src/Backoffice/src/Mvc/Index/IndexController.php
@@ -11,6 +11,7 @@ class IndexController extends BaseController
     public function indexAction()
     {
         $this->getView()->setHeading($this->translate('index.title'));
+        $this->getControllerResponse()->setRedirect($this->getPathHelper()->setController('cmsmenu')->getPath());
     }
 
 }
